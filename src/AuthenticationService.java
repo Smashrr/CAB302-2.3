f51b523fc61cf27c46380f284a00e376d2ee95ac
Software Sam <georgesamsquo@hotmail.com>
2024-03-09 12:56:00 +1000

    Implemented signUp and logIn

--- src/AuthenticationService.java
+++ src/AuthenticationService.java
@@ -12,11 +12,30 @@ public class AuthenticationService implements IAuthenticationService {
     }
 
     // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
+    @Override
     public User signUp(String username, String password){
         Iterator i = users.iterator();
         while(i.hasNext()){
             User existsAlready = (User) i.next();
+            if(existsAlready.getUsername().equals(username)){
+                return null;
+            }
         }
+        User newUser = new User(username, password);
+        users.add(newUser);
+        return newUser;
     }
     // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
+    @Override
+    public User logIn(String username, String password){
+        Iterator i = users.iterator();
+        while(i.hasNext()){
+            User user = (User) i.next();
+            if(user.getUsername().equals(username) && user.getPassword().equals(password)){
+                return user;
+            }
+        }
+        return null;
+
+    }
 }
